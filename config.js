45047add501fd21a1770df95c57d9c30d0a94df6
FishViruss <166916782+FishViruss@users.noreply.github.com>
2024-12-31 19:49:43 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'Quynh Anh:3';
+const giftUrl = 'https://zalo.me/0337885300';
+const eventName = 'Chúc Mừng Năm Mới';
+const titleCard = 'Tặng Quỳnh Anh - Quýt';
+const contentCard = 'Quỳnh Anh Nè: Năm mới đã đến, mang theo những tia sáng đầu tiên của bình minh. Mong những điều tốt đẹp nhất sẽ luôn đồng hành cùng bà 🌸. Giữa những vì sao lấp lánh giữa dải ngân hà, Quỳnh Anh là ánh sáng rực rỡ nhất.💓. Hãy luôn mỉm cười, vì nụ cười của Quỳnh Anh đẹp lắm 💓. Chúc Quỳnh Anh năm mới an lành, hạnh phúc, và luôn tràn đầy yêu thương.🌷🌹💐'
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'QuynhAnh.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
